fb8c1ed7856f3f4e3005b6f92283a556fdd5a719
amin <aminwastaken@gmail.com>
2021-09-21 12:35:16 +0200

    Empty elastic search request

--- client/src/App.js
+++ client/src/App.js
@@ -1,25 +1,32 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import { useState } from "react";
+import axios from "axios";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
-}
+const App = () => {
+  // (async () => {  const result = await client.search({
+  //   index: 'movies',
+  //   body: {
+  //     query: {
+  //       match: { hello: 'world' }
+  //     }
+  //   }
+  // })})();
+  const query = {
+    match: {
+      overview: "adventure",
+    },
+  };
+  const [response, setResponse] = useState({});
+  axios
+    .get("http://0.0.0.0:9200/movies/_search", {
+      data: JSON.stringify(query),
+    })
+    .then((res) => {
+      setResponse(res);
+      console.log(res);
+    });
+
+  return <div className="App">{JSON.stringify(response)}</div>;
+};
 
 export default App;
