1dbdbc00c4eefa6042a41784855188f93529e374
amin <aminwastaken@gmail.com>
2021-09-21 12:37:02 +0200

    Fixed state issue

--- client/src/App.js
+++ client/src/App.js
@@ -1,5 +1,5 @@
 import "./App.css";
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import axios from "axios";
 
 const App = () => {
@@ -17,14 +17,17 @@ const App = () => {
     },
   };
   const [response, setResponse] = useState({});
-  axios
-    .get("http://0.0.0.0:9200/movies/_search", {
-      data: JSON.stringify(query),
-    })
-    .then((res) => {
-      setResponse(res);
-      console.log(res);
-    });
+
+  useEffect(() => {
+    axios
+      .get("http://0.0.0.0:9200/movies/_search", {
+        data: JSON.stringify(query),
+      })
+      .then((res) => {
+        setResponse(res);
+        console.log(res);
+      });
+  });
 
   return <div className="App">{JSON.stringify(response)}</div>;
 };
